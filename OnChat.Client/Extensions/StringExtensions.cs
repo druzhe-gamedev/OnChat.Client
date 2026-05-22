1f9fe4a0f77438e7c1fbdae0595559a003ac0c72
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-22 18:21:53 +0300

    add StringExtensions for parsing arguments

---
+++ OnChat.Client/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Extensions;
+
+public static class StringExtensions
+{
+    extension(string str)
+    {
+        public static bool IsNullOrWhiteSpaces(params string?[] strings) => strings.Any(string.IsNullOrWhiteSpace);
+    }
+}
