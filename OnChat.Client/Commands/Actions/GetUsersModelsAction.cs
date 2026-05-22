899ba8c79b432cf8acdf9ed454a8defeac6a19fb
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-22 18:19:31 +0300

    add users fetch action

---
+++ OnChat.Client/Commands/Actions/GetUsersModelsAction.cs
@@ -0,0 +1,32 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using Client.Connection;
+using Client.User;
+using Microsoft.Extensions.Logging;
+using OnChat.Protocol.Packets;
+using OnChat.Shared.Users;
+
+namespace Client.Commands.Actions;
+
+public class GetUsersModelsAction(ChatConnection connection, UsersProvider usersProvider, ILogger<GetUsersModelsAction> logger)
+    : AsynchronousCommandLineAction
+{
+    public override async Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken = default)
+    {
+        IResponse response = await connection.Request<IResponse>(new GetUsersModelsPacket(Guid.Empty), cancellationToken);
+
+        if (response is not ReceiveUsersModelsPacket usersPacket)
+        {
+            logger.LogError("Error!");
+            return 1;
+        }
+
+        foreach (UserModel userModel in usersPacket.UsersModels)
+        {
+            usersProvider.Users.AddOrUpdate(userModel.UserId, userModel, (_, _) => userModel);
+            logger.LogInformation("[{UserModelUserId}] {UserModelUsername}", userModel.UserId, userModel.Username);
+        }
+        
+        return 0;
+    }
+}
