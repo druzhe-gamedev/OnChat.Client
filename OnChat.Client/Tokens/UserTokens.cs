398d310eea0b16a93487903ecb1596d02e1ff959
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-22 18:22:47 +0300

    add UsersProvider and UserTokens

---
+++ OnChat.Client/Tokens/UserTokens.cs
@@ -0,0 +1,9 @@
+﻿using OnChat.Shared.Auth;
+
+namespace Client.Tokens;
+
+public record TokensState;
+
+public record TokensEmpty : TokensState;
+
+public record UserTokens(TokenModel AccessToken, TokenModel RefreshToken) : TokensState;
---
+++ OnChat.Client/User/UsersProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+using OnChat.Shared.Users;
+
+namespace Client.User;
+
+public class UsersProvider
+{
+    public readonly ConcurrentDictionary<Guid, UserModel> Users = [];
+}
