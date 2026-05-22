9006cfa0378ca144002648e090dd9502125c71df
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-22 18:17:39 +0300

    add login on successful response

--- OnChat.Client/Commands/Actions/AuthenticationAction.cs
+++ OnChat.Client/Commands/Actions/AuthenticationAction.cs
@@ -1,32 +1,44 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Invocation;
 using Client.Connection;
+using Client.Extensions;
+using Client.Tokens;
+using Microsoft.Extensions.Logging;
 using OnChat.Protocol.Packets;
+using OnChat.Shared;
 using OnChat.Shared.Auth;
-using Serilog;
 
 namespace Client.Commands.Actions;
 
-public class AuthenticationAction(ChatConnection connection) : AsynchronousCommandLineAction
+public class AuthenticationAction(
+    ChatConnection connection,
+    TokensService tokensService,
+    ILogger<AuthenticationAction> logger
+) : AsynchronousCommandLineAction
 {
     public override async Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken = default)
     {
-        string? login = parseResult.GetValue<string>("login");
-        string? password = parseResult.GetValue<string>("pwd");
+        string? login = parseResult.GetValue<string>("-l");
+        string? password = parseResult.GetValue<string>("-p");
 
-        if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+        if (string.IsNullOrWhiteSpaces(login, password))
         {
-            Log.Error("login and password must have non-zero length");
+            logger.LogError("login and password must have non-zero length");
             return 1;
         }
         
         AuthenticationPacket packet = new(Guid.Empty, login, password);
         
         IResponse response = await connection.Request<IResponse>(packet, cancellationToken: cancellationToken);
-        
-        if(response is TokensPacket success)
+
+        switch (response)
         {
-            Console.WriteLine($"Logged in {success.AccessToken}\n{success.RefreshToken}");
+            case TokensPacket tokens:
+                tokensService.Login(tokens.AccessToken, tokens.RefreshToken);
+                break;
+            case FailureResponse failure:
+                Console.WriteLine(failure.Description);
+                return 1;
         }
 
         return 0;
